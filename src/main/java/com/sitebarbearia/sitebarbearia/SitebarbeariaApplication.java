bf4049409ce766b06375768a0c4bbbd065c33b5b
Kauan-Code19 <pkauprofissional@gmail.com>
2023-12-14 18:14:21 -0300

    configurações iniciais do projeto

---
+++ src/main/java/com/sitebarbearia/sitebarbearia/SitebarbeariaApplication.java
@@ -0,0 +1,13 @@
+package com.sitebarbearia.sitebarbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitebarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitebarbeariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitebarbearia/sitebarbearia/SitebarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitebarbearia.sitebarbearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitebarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
